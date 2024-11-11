5d591dc8e925b753182d1762175ba7e3ed360724
Kang San KIM <119637398+yoojadoni@users.noreply.github.com>
2024-11-11 14:44:44 +0900

    Update CustomAuthenticationProvider.java

--- config/CustomAuthenticationProvider.java
+++ config/CustomAuthenticationProvider.java
@@ -1,1 +1,22 @@
 
+import config/CustomPasswordEncoder;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider{
+
+  // 유저 정보 조회
+  private final MemberService memberService;
+  // 패스워드 암호화
+  private final CustomPasswordEncoder passwordEncoder;
+
+  private final CustomUserDetailService userDetailService;
+
+  private final ObjectMapper objectMapper;
+
+  @Override
+  public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+    String userId = authentication.getName();
+    String password = 
+  }
+  
+}
